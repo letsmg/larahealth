d870d2fefcae07a00a020f9e1196ab2ca47fed61
Luiz Eduardo <luiztsilva@protonmail.com>
2026-05-23 16:44:32 -0300

    fix: corrige caminho das imagens e adiciona favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="robots" content="index, follow">
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
 
         {{-- SEO Meta Tags Base --}}
         <title>@yield('title', 'Blink - Sistema Inteligente de Gestão de Clínicas e Consultórios')</title>
@@ -16,13 +17,13 @@
         <meta property="og:description" content="@yield('og:description', 'Blink é o sistema inteligente de gestão de clínicas e consultórios. Agende consultas, gerencie pacientes e organize sua agenda com praticidade e segurança.')" />
         <meta property="og:type" content="@yield('og:type', 'website')" />
         <meta property="og:url" content="@yield('og:url', url()->current())" />
-        <meta property="og:image" content="@yield('og:image', asset('storage/og-image.png'))" />
+        <meta property="og:image" content="@yield('og:image', asset('og-image.png'))" />
 
         {{-- Twitter Cards --}}
         <meta name="twitter:card" content="@yield('twitter:card', 'summary_large_image')" />
         <meta name="twitter:title" content="@yield('twitter:title', 'Blink - Sistema Inteligente de Gestão de Clínicas e Consultórios')" />
         <meta name="twitter:description" content="@yield('twitter:description', 'Blink é o sistema inteligente de gestão de clínicas e consultórios. Agende consultas, gerencie pacientes e organize sua agenda com praticidade e segurança.')" />
-        <meta name="twitter:image" content="@yield('twitter:image', asset('storage/og-image.png'))" />
+        <meta name="twitter:image" content="@yield('twitter:image', asset('og-image.png'))" />
 
         @fonts
 
